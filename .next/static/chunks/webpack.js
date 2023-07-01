ca5e21a3e47535d59ec9593e7876ccd46ec1026a
unknown <abasian.arman@gmail.com>
2023-06-30 20:40:29 -0700

    some changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a44ece9525b01c7c"; }
+/******/ 		__webpack_require__.h = function() { return "93badf7f539c8824"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
